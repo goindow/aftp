de95ab63f496b98c492242ef2fd358f9cc6be122
hyb <76788424@qq.com>
2021-06-24 16:13:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,93 @@
+# aftp
+FTP 客户端非交互式命令行脚本
+
+## 使用场景
+- 边界接入服务中，为前/后置机与 ftp server 数据交换提供自动化脚本支持，帮助隔绝的两个网络实现基于文件的数据同步
+
+## 功能列表
+- 批量上传
+- 批量下载
+- 列出 ftp server 目录内容
+- 发送命令至  fpt server 执行
+
+## 依赖说明
+- ftp
+
+## 
+```shell
+Usage: aftp COMMAND [OPTS...] [ARGS...]
+
+  FTP client non interactive command line script.
+
+Commands:
+  push            Push one or more files to FTP server
+  pull            Pull one or more files from FTP server
+  exec            Execute the command on the FTP server
+  ls              List the contents of FTP server directory
+
+Run 'aftp help COMMAND' for more details on a command.               Copy the ssh-key to one or more sessions
+```
+
+## 批量上传
+- 使用通配符上传，必须放在引号内，避免被提前扩展，如 "*.data"，否则通配符将被扩展，并且是基于当前目录的，而不是 ftp_local 本地工作目录
+- 默认使用 ascii 文本传输模式上传，如果是图片、可执行文件等，则必须使用 binary 二进制传输模式上传
+```shell
+Usage: aftp push -h host [-p port] -u user [-P password] [-m transfer_mode] 
+                 -l local_directory -r remote_directory files...
+
+  Push one or more files to FTP server.
+
+Options:
+  -h            FTP server host name
+  -l            Local working directory(lcd)
+  -m            Set transfer mode(default "ascii", only be <ascii|binary>)
+  -p            FTP server port(default 21)
+  -P            User password
+  -r            Remote working directory(cd)
+  -u            User name
+```
+
+## 批量下载
+- 使用通配符下载，必须放在引号内，避免被提前扩展，如 "*.data"，否则通配符将被扩展，并且是基于当前目录的，而不是 ftp_remote 远程工作目录
+- 默认使用 ascii 文本传输模式下载，如果是图片、可执行文件等，则必须使用 binary 二进制传输模式下载
+```shell
+Usage: aftp pull -h host [-p port] -u user [-P password] [-m transfer_mode] 
+                 -l local_directory -r remote_directory files...
+
+  Pull one or more files from FTP server.
+
+Options:
+  -h            FTP server host name
+  -l            Local working directory(lcd)
+  -m            Set transfer mode(default "ascii", only be <ascii|binary>)
+  -p            FTP server port(default 21)
+  -P            User password
+  -r            Remote working directory(cd)
+  -u            User name
+```
+
+## 发送服务器命令
+```shell
+Usage: aftp exec -h host [-p port] -u user [-P password] COMMAND
+
+  Execute the command on the FTP server.
+
+Options:
+  -h            FTP server host name
+  -p            FTP server port(default 21)
+  -P            User password
+  -u            User name
+```
+
+## 列出服务器目录内容
+```shell
+Usage: aftp ls -h host [-p port] -u user [-P password] [DIR(default /)]
+
+  List the contents of FTP server directory.
+
+Options:
+  -h            FTP server host name
+  -p            FTP server port(default 21)
+  -P            User password
+  -u            User name
+```
---
+++ aftp.sh
@@ -0,0 +1,255 @@
+#!/bin/bash
+set -f
+# 配置文件，指定下列配置默认值，若命令行中传递对应参数则覆盖配置文件中相应的配置
+fpt_conf=
+# 认证信息
+ftp_host=
+ftp_port=21
+ftp_user=
+ftp_pass=
+# 本地工作目录(ftp>lcd)，push-从该目录寻找文件上传; pull-文件下载到该目录
+ftp_local=
+# 远程工作目录(ftp>cd)，push-文件上传到该目录; pull-从该目录寻找文件下载
+ftp_remote=
+# 传输模式，ascii|binary
+ftp_transfer_mode=ascii
+
+function usage() {
+  cat << 'EOF'
+Usage: aftp COMMAND [OPTS...] [ARGS...]
+
+  FTP client non interactive command line script.
+
+Commands:
+  push            Push one or more files to FTP server
+  pull            Pull one or more files from FTP server
+  exec            Execute the command on the FTP server
+  ls              List the contents of FTP server directory
+
+Run 'aftp help COMMAND' for more details on a command.
+EOF
+}
+
+function usage_ls() {
+  cat << 'EOF'
+Usage: aftp ls -h host [-p port] -u user [-P password] [DIR(default /)]
+
+  List the contents of FTP server directory.
+
+Options:
+  -h            FTP server host name
+  -p            FTP server port(default 21)
+  -P            User password
+  -u            User name
+EOF
+}
+
+function usage_exec() {
+  cat << 'EOF'
+Usage: aftp exec -h host [-p port] -u user [-P password] COMMAND
+
+  Execute the command on the FTP server.
+
+Options:
+  -h            FTP server host name
+  -p            FTP server port(default 21)
+  -P            User password
+  -u            User name
+EOF
+}
+
+function usage_push() {
+  cat << 'EOF'
+Usage: aftp push -h host [-p port] -u user [-P password] [-m transfer_mode] 
+                 -l local_directory -r remote_directory files...
+
+  Push one or more files to FTP server.
+
+Options:
+  -h            FTP server host name
+  -l            Local working directory(lcd)
+  -m            Set transfer mode(default "ascii", only be <ascii|binary>)
+  -p            FTP server port(default 21)
+  -P            User password
+  -r            Remote working directory(cd)
+  -u            User name
+EOF
+}
+
+function usage_pull() {
+  cat << 'EOF'
+Usage: aftp pull -h host [-p port] -u user [-P password] [-m transfer_mode] 
+                 -l local_directory -r remote_directory files...
+
+  Pull one or more files from FTP server.
+
+Options:
+  -h            FTP server host name
+  -l            Local working directory(lcd)
+  -m            Set transfer mode(default "ascii", only be <ascii|binary>)
+  -p            FTP server port(default 21)
+  -P            User password
+  -r            Remote working directory(cd)
+  -u            User name
+EOF
+}
+
+# 通知
+# $1 通知类型
+# $2 通知内容
+function dialog() {
+  case $1 in
+    fatal) printf '%s\n' "$2" && exit 1;;
+    error) printf '%s\n\n%s\n' "$2" 'For more details, see "aftp help".' && exit 1;;
+    info)  printf '%s\n' "$2";;
+    ok)    echo 'OK.';;
+    exit)  echo 'exited.';;
+  esac
+  exit 0
+}
+
+# 判断数组是否包含某元素
+# $1     被检查元素
+# $2...  数组
+function array_has() {
+  test $# -lt 2 && return 1
+  sub=$1 && shift
+  echo $@ | sed 's/ /\'$'\n/g' | xargs -I % bash -c "test % == $sub && echo 0 || echo 1" | grep 0 &> /dev/null
+}
+
+# args 解析，获取文件 basename
+function args() {
+  echo $@ | sed 's/ /\'$'\n/g' | xargs -I % basename % | tr '\n' ' '
+}
+
+function opts_lr() {
+  test -z $ftp_local && dialog error 'Requires an option -l, specify the local working directory.'
+  test -z $ftp_remote && dialog error 'Requires an option -r, specify the remote working directory.'
+}
+
+function opts_m() {
+  test -z $mode && return
+  array_has $mode ascii binary && ftp_transfer_mode=$mode || dialog error 'Invalid value for option -m, the transfer mode only be acsii or binary.'
+}
+
+# getopts 解析
+# $@, 参数(opts... args...), -h host [-p port] -u user [-p password] args...
+# @return $?, 调用方 shift 剔除 getopts 参数数量
+function opts() {
+  while getopts 'h:p:u:P:l:r:m:' options; do
+    case $options in
+      # 认证信息
+      h) host=$OPTARG;;
+      p) port=$OPTARG;;
+      u) user=$OPTARG;;
+      P) pass=$OPTARG;;
+      # 工作目录
+      l) local=$OPTARG;;
+      r) remote=$OPTARG;;
+      # 传输模式
+      m) mode=$OPTARG;;
+    esac
+  done
+  test ! -z $host && ftp_host=$host || dialog error 'Requires an option -h, specify host.'
+  test ! -z $port && ftp_port=$port
+  test ! -z $user && ftp_user=$user || dialog error 'Requires an option -u, specify user.'
+  test ! -z $pass && ftp_pass=$pass
+  test ! -z $local && ftp_local=$local
+  test ! -z $remote && ftp_remote=$remote
+  opts_m $mode
+  return $(($OPTIND - 1))
+}
+
+# 列出文件和目录
+# ./aftp.sh ls -h 10.20.11.46 -p 21 -u ftp1 -P 123456 /data/
+function ls() {
+  opts $@ || shift $?
+  ftp -inp << EOF
+open $ftp_host $ftp_port
+user $ftp_user $ftp_pass
+$(test ! -z $1 && echo "cd $1")
+ls
+close
+bye
+EOF
+}
+
+# 执行 ftp 命令
+# ./aftp.sh exec -h 10.20.11.46 -p 21 -u ftp1 -P 123456 pwd
+# ftp -inp
+#   -i, do not prompt during multiple file transfers(prompt)
+#   -n, do not automatically login to the remote system(user $user $password)
+#   -p, enable passive mode transfer
+function exec() {
+  opts $@ || shift $?
+  test $# -le 0 && dialog error 'Requires one command as the argument.'
+  ftp -inp << EOF
+open $ftp_host $ftp_port
+user $ftp_user $ftp_pass
+$@
+close
+bye
+EOF
+}
+
+# 批量上传文件
+# aftp push -h host [-p port] -u user [-P password] -l local -r remote files...
+#
+# ./aftp.sh push -h 10.20.11.46 -p 21 -u ftp1 -P 123456 -l /usr/local/var/sh/workspace/aftp/test/send/ -r /data/ 1.data
+# 使用通配符，必须放在引号内，避免被提前扩展，如 "*.data"，否则通配符将被扩展，并且是基于当前目录的，而不是 ftp_local 本地工作目录
+# ./aftp.sh push -h 10.20.11.46 -p 21 -u ftp1 -P 123456 -l /usr/local/var/sh/workspace/aftp/test/send/ -r / "*"
+# 使用 binary 传输模式上传
+# ./aftp.sh push -h 10.20.11.46 -p 21 -u ftp1 -P 123456 -l /usr/local/var/sh/workspace/aftp/test/send/ -r / -m binary test.png
+function push() {
+  opts $@ || shift $?
+  opts_lr
+  ftp -inp << EOF &> /dev/null
+open $ftp_host $ftp_port
+user $ftp_user $ftp_pass
+lcd $ftp_local
+cd $ftp_remote
+$ftp_transfer_mode
+mput $(args $@)
+close
+bye
+EOF
+  dialog ok
+}
+
+# 批量下载文件
+# ./aftp.sh pull -h 10.20.11.46 -p 21 -u ftp1 -P 123456 -l /usr/local/var/sh/workspace/aftp/test/recv/ -r /data/ 1.data
+# 使用通配符，必须放在引号内，避免被提前扩展，如 "*.data"，否则通配符将被扩展，并且是基于当前目录的，而不是 ftp_remote 远程工作目录
+# ./aftp.sh pull -h 10.20.11.46 -p 21 -u ftp1 -P 123456 -l /usr/local/var/sh/workspace/aftp/test/recv/ -r / "*"
+# 使用 binary 传输模式下载
+# ./aftp.sh pull -h 10.20.11.46 -p 21 -u ftp1 -P 123456 -l /usr/local/var/sh/workspace/aftp/test/recv/ -r / -m binary test.png
+function pull() {
+  opts $@ || shift $?
+  opts_lr
+  ftp -inp << EOF &> /dev/null
+open $ftp_host $ftp_port
+user $ftp_user $ftp_pass
+lcd $ftp_local
+cd $ftp_remote
+$ftp_transfer_mode
+mget $(args $@)
+close
+bye
+EOF
+  dialog ok
+}
+
+function help() {
+  test -z $1 && usage && exit
+  type -t usage_$1 &> /dev/null && usage_$1 || dialog error "$1: command not found."
+}
+
+# main
+test ! -x "$(command -v ftp)" && dialog fatal 'ftp: command not found.'
+case $1 in
+  ls) shift && ls $@;;
+  exec) shift && exec $@;;
+  push) shift && push $@;;
+  pull) shift && pull $@;;
+  help) shift && help $1;;
+  *) usage;;
+esac
